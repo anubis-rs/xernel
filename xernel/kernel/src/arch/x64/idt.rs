dc56019fc0e594be3a17c09b1f9767ed8a45ee04
Raphael O <raphael.ob@protonmail.com>
2022-09-11 22:02:02 +0200

    Set general handler for all interrupts

--- xernel/kernel/src/arch/x64/idt.rs
+++ xernel/kernel/src/arch/x64/idt.rs
@@ -1,13 +1,25 @@
 use lazy_static::lazy_static;
-use x86_64::structures::idt::InterruptDescriptorTable;
+use x86_64::set_general_handler;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+use crate::{println, print};
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
-        let idt = InterruptDescriptorTable::new();
+        let mut idt = InterruptDescriptorTable::new();
+        set_general_handler!(&mut idt, interrupt_handler);
         idt
     };
 }
 
 pub fn init() {
     IDT.load();
+}
+
+fn interrupt_handler(stack_frame: InterruptStackFrame, index: u8, error_code: Option<u64>) {
+
+    println!("IP: {:?}", stack_frame.instruction_pointer);
+    println!("index: {}", index);
+    println!("error_code: {}", error_code.unwrap_or(0));
+
 }
