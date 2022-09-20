12a69bfaf2d3ead158d44f03ebee031960ac4f37
Raphael O <raphael.ob@protonmail.com>
2022-09-20 11:01:08 +0200

    Implement page fault handler (more infos)

--- xernel/kernel/src/arch/x64/idt.rs
+++ xernel/kernel/src/arch/x64/idt.rs
@@ -2,6 +2,8 @@ use core::arch::asm;
 use lazy_static::lazy_static;
 use x86_64::set_general_handler;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::registers::control::Cr2;
+use x86_64::structures::idt::PageFaultErrorCode;
 
 use crate::{print, println};
 
@@ -9,6 +11,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         set_general_handler!(&mut idt, interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 }
@@ -23,5 +26,21 @@ fn interrupt_handler(stack_frame: InterruptStackFrame, index: u8, error_code: Op
     println!("error_code: {}", error_code.unwrap_or(0));
     unsafe {
         asm!("hlt");
+    }
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    loop {
+        unsafe {
+            asm!("hlt");
+        }
     }
 }
