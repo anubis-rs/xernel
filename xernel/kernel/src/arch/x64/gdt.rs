6917d2cf26b97454cb70f05ce81108dc3ae8e9a1
Markus Pfeifenberger <pfeifenberger.markus2003@gmail.com>
2022-09-27 19:18:25 +0200

    fix gdt

--- xernel/kernel/src/arch/x64/gdt.rs
+++ xernel/kernel/src/arch/x64/gdt.rs
@@ -1,4 +1,4 @@
-use x86_64::instructions::segmentation::{Segment, CS};
+use x86_64::instructions::segmentation::{Segment, CS, DS, ES, FS, GS, SS};
 use x86_64::instructions::tables::load_tss;
 use x86_64::structures::gdt::SegmentSelector;
 use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable};
@@ -25,19 +25,23 @@ lazy_static! {
     static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
         let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let data_selector = gdt.add_entry(Descriptor::kernel_data_segment());
         let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
         (
             gdt,
             Selectors {
                 code_selector,
+                data_selector,
                 tss_selector,
             },
         )
     };
 }
 
+#[derive(Debug)]
 struct Selectors {
     code_selector: SegmentSelector,
+    data_selector: SegmentSelector,
     tss_selector: SegmentSelector,
 }
 
@@ -45,6 +49,12 @@ pub fn init() {
     GDT.0.load();
     unsafe {
         CS::set_reg(GDT.1.code_selector);
+        SS::set_reg(GDT.1.data_selector);
+        DS::set_reg(GDT.1.data_selector);
+        ES::set_reg(GDT.1.data_selector);
+        FS::set_reg(GDT.1.data_selector);
+        GS::set_reg(GDT.1.data_selector);
+
         load_tss(GDT.1.tss_selector);
     }
 }
