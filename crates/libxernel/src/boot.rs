8229c37faaf83b586b1f753cfd6f328789b3454c
Raphael O <raphael.ob@protonmail.com>
2022-09-29 18:37:33 +0200

    Rework InitAtBoot to old way of working

--- crates/libxernel/src/boot.rs
+++ crates/libxernel/src/boot.rs
@@ -1,43 +1,43 @@
-use core::{ops::{Deref, DerefMut}, cell::UnsafeCell, mem::MaybeUninit, sync::atomic::{AtomicBool, Ordering}, panic};
+use core::{
+    ops::{Deref, DerefMut},
+    panic,
+};
 
-pub struct InitAtBoot<T> {
-    is_set: AtomicBool,
-    data: UnsafeCell<MaybeUninit<T>>
+pub enum InitAtBoot<T> {
+    Initialized(T),
+    Uninitialized,
 }
-
-impl<T> InitAtBoot<T> {
-
-    pub const fn new() -> Self {
-        Self {
-            is_set: AtomicBool::new(false),
-            data: UnsafeCell::new(MaybeUninit::uninit())
-        }
-    }
-
-    pub fn set_once(&self, val: T) {
-        if !self.is_set.load(Ordering::Acquire) {
-            unsafe { (*self.data.get()).as_mut_ptr().write(val); }
-            self.is_set.store(true, Ordering::Release);
-        } else {
-            panic!("Value already set");
-        }
-    }
-
-}
-
-unsafe impl<T> Send for InitAtBoot<T> {}
-unsafe impl<T> Sync for InitAtBoot<T> {}
 
 impl<T> Deref for InitAtBoot<T> {
     type Target = T;
 
-    fn deref(&self) -> &Self::Target {
-        unsafe { &*(*self.data.get()).as_ptr() }
+    fn deref(&self) -> &T {
+        match self {
+            InitAtBoot::Initialized(x) => x,
+            InitAtBoot::Uninitialized => {
+                #[cfg(debug_assertions)]
+                panic!("tried to access boot resource that is not initialized");
+                #[cfg(not(debug_assertions))]
+                unsafe {
+                    core::hint::unreachable_unchecked()
+                }
+            }
+        }
     }
 }
 
 impl<T> DerefMut for InitAtBoot<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { &mut *(*self.data.get()).as_mut_ptr() }
+    fn deref_mut(&mut self) -> &mut T {
+        match self {
+            InitAtBoot::Initialized(x) => x,
+            InitAtBoot::Uninitialized => {
+                #[cfg(debug_assertions)]
+                panic!("tried to access boot resource that is not initialized");
+                #[cfg(not(debug_assertions))]
+                unsafe {
+                    core::hint::unreachable_unchecked()
+                }
+            }
+        }
     }
 }
