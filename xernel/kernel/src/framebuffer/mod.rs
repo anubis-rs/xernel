af71fd0d2b23a34bcbdc4d27b2c73636874e6bbd
Raphael O <raphael.ob@protonmail.com>
2022-09-29 11:48:10 +0200

    Add puts function, rename printc -> putc, function into struct

--- xernel/kernel/src/framebuffer/mod.rs
+++ xernel/kernel/src/framebuffer/mod.rs
@@ -6,16 +6,28 @@ use crate::framebuffer::font::FONT;
 use libxernel::ticket::TicketMutex;
 use limine::{LimineFramebuffer, LimineFramebufferRequest};
 
-struct Framebuffer {
+pub struct Framebuffer {
     cursor: u64,
     char_current_line: u8,
+    color: Color,
+}
+
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
 }
 
 static FRAMEBUFFER_REQUEST: LimineFramebufferRequest = LimineFramebufferRequest::new(0);
 
-static FRAMEBUFFER: TicketMutex<Framebuffer> = TicketMutex::new(Framebuffer {
+pub static FRAMEBUFFER: TicketMutex<Framebuffer> = TicketMutex::new(Framebuffer {
     cursor: 0,
     char_current_line: 0,
+    color: Color {
+        r: 0xff,
+        g: 0xff,
+        b: 0xff,
+    },
 });
 
 lazy_static! {
@@ -31,80 +43,100 @@ lazy_static! {
     };
 }
 
-pub unsafe fn printc(character: char) {
-    debug_assert!(character.is_ascii());
+impl Framebuffer {
+    unsafe fn putc(&mut self, character: char) {
+        debug_assert!(character.is_ascii());
 
-    let mut fb = FRAMEBUFFER.lock();
+        let c = character as u8;
 
-    let c = character as u8;
+        let address = FRAMEBUFFER_DATA.address.as_mut_ptr().unwrap().cast::<u8>();
 
-    let address = FRAMEBUFFER_DATA.address.as_mut_ptr().unwrap().cast::<u8>();
+        let mut index: u16 = 0;
 
-    let mut index: u16 = 0;
+        if u64::from(self.char_current_line) == (FRAMEBUFFER_DATA.width / 9) {
+            self.char_current_line = 0;
 
-    if u64::from(fb.char_current_line) == (FRAMEBUFFER_DATA.width / 9) {
-        fb.char_current_line = 0;
-
-        fb.cursor -= fb.cursor % FRAMEBUFFER_DATA.pitch;
-        fb.cursor += FRAMEBUFFER_DATA.pitch * 17;
-    }
-
-    if fb.cursor >= fb_length() - FRAMEBUFFER_DATA.pitch * 17 {
-        fb.cursor -= FRAMEBUFFER_DATA.pitch * 17;
-
-        copy(
-            address.add((FRAMEBUFFER_DATA.pitch * 17) as usize),
-            address,
-            (fb_length() - FRAMEBUFFER_DATA.pitch * 17) as usize,
-        );
-
-        for i in 0..FRAMEBUFFER_DATA.pitch * 17 {
-            address.add((fb.cursor + i) as usize).write_volatile(0x00);
+            self.cursor -= self.cursor % FRAMEBUFFER_DATA.pitch;
+            self.cursor += FRAMEBUFFER_DATA.pitch * 17;
         }
-    }
 
-    if character == '\n' {
-        fb.cursor -= fb.cursor % FRAMEBUFFER_DATA.pitch;
-        fb.cursor += FRAMEBUFFER_DATA.pitch * 17;
+        if self.cursor >= self.length() - FRAMEBUFFER_DATA.pitch * 17 {
+            self.cursor -= FRAMEBUFFER_DATA.pitch * 17;
 
-        fb.char_current_line = 0;
+            copy(
+                address.add((FRAMEBUFFER_DATA.pitch * 17) as usize),
+                address,
+                (self.length() - FRAMEBUFFER_DATA.pitch * 17) as usize,
+            );
 
-        return;
-    }
-
-    if character == '\t' {
-        fb.cursor += 32 * 4 * 4;
-
-        return;
-    }
-
-    if character != ' ' {
-        index = (c as u16 - 32) * 16;
-    }
-
-    fb.char_current_line += 1;
-
-    for i in index..index + 16 {
-        let bitmap: u8 = FONT[i as usize];
-
-        for j in 0..8 {
-            if (bitmap & (1 << (7 - j))) >= 1 {
-                address.add(fb.cursor as usize).write_volatile(0xff);
-                address.add((fb.cursor + 1) as usize).write_volatile(0xff);
-                address.add((fb.cursor + 2) as usize).write_volatile(0xff);
+            for i in 0..FRAMEBUFFER_DATA.pitch * 17 {
+                address.add((self.cursor + i) as usize).write_volatile(0x00);
             }
-            fb.cursor += (FRAMEBUFFER_DATA.bpp / 8) as u64;
         }
-        fb.cursor -= FRAMEBUFFER_DATA.bpp as u64;
-        fb.cursor += FRAMEBUFFER_DATA.pitch;
+
+        if character == '\n' {
+            self.cursor -= self.cursor % FRAMEBUFFER_DATA.pitch;
+            self.cursor += FRAMEBUFFER_DATA.pitch * 17;
+
+            self.char_current_line = 0;
+
+            return;
+        }
+
+        if character == '\t' {
+            self.cursor += 32 * 4 * 4;
+
+            return;
+        }
+
+        if character != ' ' {
+            index = (c as u16 - 32) * 16;
+        }
+
+        self.char_current_line += 1;
+
+        for i in index..index + 16 {
+            let bitmap: u8 = FONT[i as usize];
+
+            for j in 0..8 {
+                if (bitmap & (1 << (7 - j))) >= 1 {
+                    address
+                        .add(self.cursor as usize)
+                        .write_volatile(self.color.b);
+                    address
+                        .add((self.cursor + 1) as usize)
+                        .write_volatile(self.color.g);
+                    address
+                        .add((self.cursor + 2) as usize)
+                        .write_volatile(self.color.r);
+                }
+                self.cursor += (FRAMEBUFFER_DATA.bpp / 8) as u64;
+            }
+            self.cursor -= FRAMEBUFFER_DATA.bpp as u64;
+            self.cursor += FRAMEBUFFER_DATA.pitch;
+        }
+
+        self.cursor += FRAMEBUFFER_DATA.bpp as u64;
+        self.cursor += (FRAMEBUFFER_DATA.bpp / 8) as u64;
+
+        self.cursor -= FRAMEBUFFER_DATA.pitch * 16;
     }
 
-    fb.cursor += FRAMEBUFFER_DATA.bpp as u64;
-    fb.cursor += (FRAMEBUFFER_DATA.bpp / 8) as u64;
+    pub fn puts(&mut self, string: &str) {
+        unsafe {
+            for c in string.chars() {
+                self.putc(c);
+            }
+        }
+    }
 
-    fb.cursor -= FRAMEBUFFER_DATA.pitch * 16;
-}
+    pub fn length(&self) -> u64 {
+        (FRAMEBUFFER_DATA.height * FRAMEBUFFER_DATA.pitch) as u64
+    }
 
-fn fb_length() -> u64 {
-    (FRAMEBUFFER_DATA.height * FRAMEBUFFER_DATA.pitch) as u64
+    pub fn set_color(&mut self, r: u8, g: u8, b: u8) {
+        self.color.r = r;
+        self.color.g = g;
+        self.color.b = b;
+    }
 }
