f6263e98cfcfb1cecff29c19abcbdb3b9c1b4780
Raphael O <raphael.ob@protonmail.com>
2022-09-29 11:47:33 +0200

    Impl debug, info, error, warning macros for better printing to framebuffer

--- xernel/kernel/src/writer.rs
+++ xernel/kernel/src/writer.rs
@@ -1,24 +1,40 @@
 use core::fmt;
 use core::fmt::Write;
 
-use crate::framebuffer::printc;
+use crate::framebuffer::FRAMEBUFFER;
 struct Writer;
 
 impl core::fmt::Write for Writer {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        for c in s.chars() {
-            unsafe {
-                printc(c);
-            }
-        }
+        let mut fb = FRAMEBUFFER.lock();
+        fb.puts(s);
         Ok(())
     }
 }
 
 #[macro_export]
 macro_rules! println {
-    () => (print!("\n"));
-    ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
+    ($($arg:tt)*) => ($crate::writer::_println(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => ($crate::writer::_log_print(format_args!($($arg)*), "DEBUG", 0x03, 0xe8, 0xfc));
+}
+
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => ($crate::writer::_log_print(format_args!($($arg)*), "INFO", 0x03, 0xfc, 0x0b));
+}
+
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => ($crate::writer::_log_print(format_args!($($arg)*), "ERROR", 0xfc, 0x03, 0x0f));
+}
+
+#[macro_export]
+macro_rules! warning {
+    ($($arg:tt)*) => ($crate::writer::_log_print(format_args!($($arg)*), "WARNING", 0xfc, 0xca, 0x03));
 }
 
 #[macro_export]
@@ -31,4 +47,27 @@ pub fn _print(args: fmt::Arguments) {
     let mut writer = Writer;
     // UNWRAP: We always return `Ok(())` inside `write_str` so this is unreachable.
     writer.write_fmt(args).unwrap();
+}
+
+#[doc(hidden)]
+pub fn _println(args: fmt::Arguments) {
+    let mut writer = Writer;
+    // UNWRAP: We always return `Ok(())` inside `write_str` so this is unreachable.
+    writer.write_fmt(args).unwrap();
+    writer.write_char('\n').unwrap();
+}
+
+#[doc(hidden)]
+pub fn _log_print(args: fmt::Arguments, level: &str, r: u8, g: u8, b: u8) {
+    let mut writer = Writer;
+
+    writer.write_char('[').unwrap();
+
+    FRAMEBUFFER.lock().set_color(r, g, b);
+    writer.write_str(level);
+    FRAMEBUFFER.lock().set_color(0xff, 0xff, 0xff);
+    writer.write_str("] ").unwrap();
+    writer.write_fmt(args).unwrap();
+
+    writer.write_char('\n').unwrap();
 }
