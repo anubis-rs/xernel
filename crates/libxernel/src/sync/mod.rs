531022c4f6c5978d986df098ca1bbaa3c63398b2
Raphael O <raphael.ob@protonmail.com>
2023-01-15 20:21:28 +0100

    Implement rwlock

--- crates/libxernel/src/sync/mod.rs
+++ crates/libxernel/src/sync/mod.rs
@@ -1,4 +1,5 @@
 pub use self::once::Once;
+pub use self::rwlock::{ReadGuard, RwLock, WriteGuard};
 pub use self::spin::{MutexGuard, Spinlock};
 pub use self::ticket::{TicketMutex, TicketMutexGuard};
 
--- crates/libxernel/src/sync/rwlock.rs
+++ crates/libxernel/src/sync/rwlock.rs
@@ -1,1 +1,99 @@
-// TODO: Implement rwlock
+use core::ops::{Deref, DerefMut};
+use core::{cell::UnsafeCell, sync::atomic::AtomicU32, sync::atomic::Ordering};
+
+pub struct RwLock<T> {
+    state: AtomicU32,
+    data: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for RwLock<T> where T: Send + Sync {}
+
+impl<T> RwLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0), // Unlocked.
+            data: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn read(&self) -> ReadGuard<T> {
+        let mut current_state = self.state.load(Ordering::Relaxed);
+
+        loop {
+            if current_state < u32::MAX {
+                match self.state.compare_exchange_weak(
+                    current_state,
+                    current_state + 1,
+                    Ordering::Acquire,
+                    Ordering::Relaxed,
+                ) {
+                    Ok(_) => return ReadGuard { rwlock: self },
+                    Err(e) => current_state = e,
+                }
+            }
+
+            while current_state == u32::MAX {
+                core::hint::spin_loop();
+                current_state = self.state.load(Ordering::Relaxed);
+            }
+        }
+    }
+
+    pub fn write(&self) -> WriteGuard<T> {
+        while let Err(_) =
+            self.state
+                .compare_exchange(0, u32::MAX, Ordering::Acquire, Ordering::Relaxed)
+        {
+            core::hint::spin_loop();
+        }
+        WriteGuard { rwlock: self }
+    }
+}
+
+pub struct ReadGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+impl<T> ReadGuard<'_, T> {
+    pub fn unlock(self) {}
+}
+
+impl<T> Deref for ReadGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.rwlock.data.get() }
+    }
+}
+
+impl<T> Drop for ReadGuard<'_, T> {
+    fn drop(&mut self) {
+        self.rwlock.state.fetch_sub(1, Ordering::Release);
+    }
+}
+
+pub struct WriteGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+impl<T> WriteGuard<'_, T> {
+    pub fn unlock(self) {}
+}
+
+impl<T> Deref for WriteGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.rwlock.data.get() }
+    }
+}
+
+impl<T> DerefMut for WriteGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.rwlock.data.get() }
+    }
+}
+
+impl<T> Drop for WriteGuard<'_, T> {
+    fn drop(&mut self) {
+        self.rwlock.state.store(0, Ordering::Release);
+    }
+}
