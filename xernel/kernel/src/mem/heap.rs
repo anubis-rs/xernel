c41765ab1ecc52e8a00b4b64d0605ca6e0a12f9f
Markus Pfeifenberger <pfeifenberger.markus2003@gmail.com>
2023-01-17 20:51:45 +0100

    use the default alloc error handler

--- xernel/kernel/src/mem/heap.rs
+++ xernel/kernel/src/mem/heap.rs
@@ -73,11 +73,6 @@ unsafe impl GlobalAlloc for Allocator {
     }
 }
 
-#[alloc_error_handler]
-fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
-    panic!("allocation error: {:?}", layout)
-}
-
 pub fn init() {
     let mut heap = HEAP.lock();
     let mut page_mapper = KERNEL_PAGE_MAPPER.lock();
