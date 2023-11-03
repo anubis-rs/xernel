358d831202705300361cf3fb20bf756f3333141f
Raphael O <raphael.ob@protonmail.com>
2023-11-03 08:32:10 +0100

    feat: Add offset attribute to filehandle struct

--- xernel/kernel/src/fs/file.rs
+++ xernel/kernel/src/fs/file.rs
@@ -5,11 +5,15 @@ use super::vnode::VNode;
 
 pub struct File {
     node: Arc<Spinlock<VNode>>,
+    offset: usize
 }
 
 impl File {
     pub fn new(node: Arc<Spinlock<VNode>>) -> Self {
-        Self { node }
+        Self { 
+            node: node,
+            offset: 0
+        }
     }
 
     pub fn get_node(&self) -> Arc<Spinlock<VNode>> {
