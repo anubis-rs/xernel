c8afa0aff8673aaf29d0d7deee771ee8e0b0f7b6
Raphael O. <raphael.ob@protonmail.com>
2023-11-04 01:19:13 +0100

    feat: Add wrmsr and rdmsr functions for amd64

--- xernel/kernel/src/arch/amd64/mod.rs
+++ xernel/kernel/src/arch/amd64/mod.rs
@@ -56,3 +56,17 @@ pub fn read_cr2() -> VirtAddr {
         VirtAddr::new(value)
     }
 }
+
+#[inline]
+pub unsafe fn wrmsr(msr: u32, value: u64) {
+    let low = value as u32;
+    let high = (value >> 32) as u32;
+    asm!("wrmsr", in("ecx") msr, in("eax") low, in("edx") high);
+}
+
+#[inline]
+pub unsafe fn rdmsr(msr: u32) -> u64 {
+    let (high, low): (u32, u32);
+    asm!("rdmsr", out("eax") low, out("edx") high, in("ecx") msr);
+    ((high as u64) << 32) | (low as u64)
+}
