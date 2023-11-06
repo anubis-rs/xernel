5a303f726b2796ebc15a525e0c92bd81ffe4d2e1
Raphael O <raphael.ob@protonmail.com>
2023-11-06 09:41:15 +0100

    fix: Get lapic address from msr properly

--- xernel/kernel/src/arch/amd64/lapic.rs
+++ xernel/kernel/src/arch/amd64/lapic.rs
@@ -16,11 +16,17 @@ pub struct LocalApic {
 impl LocalApic {
     pub fn new() -> Self {
         let mut mapper = KERNEL_PAGE_MAPPER.lock();
-
-        // FIXME: base address of by 900
-        let apic_base = unsafe {
+        
+        let mut apic_base = unsafe {
             rdmsr(IA32_APIC_BASE_MSR)
         };
+
+        // INFO: IA32_APIC_BASE_MSR contains to flags on bit 8 and bit 11
+        // BSP flag, bit 8 ⎯ Indicates if the processor is the bootstrap processor (BSP).
+        // APIC Global Enable flag, bit 11 ⎯ Enables or disables the local APIC
+        // To get the local apic base address, bit range 12 - 35, we set the flag bits to zero 
+        apic_base &= !(1 << 8);
+        apic_base &= !(1 << 11);
 
         debug!("apic base: {:x}", apic_base);
 
